47eb300283b14f66a52326f9d1d2ef3e094be22e
fmvaldezg <fmvaldezg@yahoo.com>
2024-09-10 12:17:43 -0400

    updated chapters

--- config.js
+++ config.js
@@ -21,10 +21,10 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: 'Main Campus',
-            image: './images/image1.png',
-            caption: 'A caption for your image',
-            website: '<a href="https://temple.maps.arcgis.com/apps/MapSeries/index.html?appid=9b76db43c88946cfa92d269463ac26dd" target="https://temple.maps.arcgis.com/apps/MapSeries/index.html?appid=9b76db43c88946cfa92d269463ac26dd">Website</a>',
-            author: 'Felipe Valdez',
+            image: './images/image1.jpg',
+            caption: 'Charles Library Building',
+            website: '<a href="https://temple.maps.arcgis.com/apps/MapSeries/index.html?appid=9b76db43c88946cfa92d269463ac26dd" target="https://temple.maps.arcgis.com/apps/MapSeries/index.html?appid=9b76db43c88946cfa92d269463ac26dd">Historical map</a>',
+            //author: 'Felipe Valdez',
             //legend: '<span style="font-size: 0.85em;"><em>Legend content</em><br></span><img src="/images/legend1.png" style="display: block; margin-top: 10px; width: 100%; max-width: 200px;">',
             description: 'Temple’s bustling Main Campus crackles with energy, thanks to its location in the heart of Philadelphia, numerous new building projects and the 10,000 students who now live on or around our increasingly residential campus. On- and off-campus activities abound, including more than 300 student organizations; Division I sporting events; and nearby restaurants, shops and entertainment.',
             location: {
@@ -58,15 +58,16 @@ var config = {
         },
         {
             id: 'chapter2',
-            alignment: 'left',
+            alignment: 'right',
             hidden: false,
-            title: 'Second slide',
+            title: 'Ambler',
             image: './images/image2.png',
-            legend: '<span style="font-size: 0.85em;"><em>Legend content</em><br></span><img src="/images/legend1.png" style="display: block; margin-top: 10px; width: 100%; max-width: 200px;">',
-            description: 'Copy these sections to add to your story.',
+            website: '<a href="https://ambler.temple.edu/" target="https://ambler.temple.edu/">Learn more</a>',
+            //legend: '<span style="font-size: 0.85em;"><em>Legend content</em><br></span><img src="/images/legend1.png" style="display: block; margin-top: 10px; width: 100%; max-width: 200px;">',
+            description: 'Beloved by generations of Temple students for its scenic beauty and nationally acclaimed gardens, Temple’s Ambler Campus fosters a close-knit community of students and faculty. Ambler students enjoy the full range of services that the suburban campus of a world-class university provides.',
             location: {
-                center: [-75.13331, 39.98741],
-                zoom: 15,
+                center: [-75.19169, 40.16554],
+                zoom: 15.9,
                 pitch: 0,
                 bearing: 0
             },
@@ -75,27 +76,106 @@ var config = {
             mapInteractive: true,
             callback: '',
             onChapterEnter: [
-                 {
-                     layer: 'pcwnet',
-                     opacity: 1
-                 }
+                 //{
+                 //    layer: 'pcwnet',
+                 //    opacity: 1
+                 //}
             ],
             onChapterExit: [
-                {
-                    layer: 'pcwnet',
-                    opacity: 0
-                }
+                //{
+                //    layer: 'pcwnet',
+                //    opacity: 0
+                //}
             ]
         },
         {
             id: 'chapter3',
+            alignment: 'left',
+            hidden: false,
+            title: 'Center City',
+            image: './images/image3.png',
+            website: '<a href="https://universitycollege.temple.edu/center-city" target="https://universitycollege.temple.edu/center-city">Website</a>', 
+            description: 'Temple University Center City (TUCC) offers the ideal learning environment for adult and continuing-education students who must often balance the demands of a career, family and school. Surrounded by the home offices of major national corporations and just across the street from Philadelphia’s City Hall. Additionally, TUCC is near many of the city’s cultural and entertainment resources. Classes are offered primarily in the evenings and on weekends.',
+            location: {
+                center: [-75.16602897661596, 39.95294851795492],
+                zoom: 17,
+                pitch: 52,
+                bearing: -36.70
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            mapInteractive: false,
+            callback: '',
+            onChapterEnter: [
+            //     {
+            //         layer: 'pcwnet',
+            //         opacity: 1
+            //     }
+            ],
+            onChapterExit: []
+        },
+        {
+            id: 'chapter4',
+            alignment: 'center',
+            hidden: false,
+            title: 'Health Sciences Center',
+            image: './images/image4.png',
+            website: '<a href="https://universitycollege.temple.edu/center-city" target="https://universitycollege.temple.edu/center-city">Website</a>', 
+            description: 'Step onto the Health Sciences Center and be immersed in the future of healthcare. Home to four of Temple’s medical-related schools and colleges, as well as the nationally ranked Temple University Hospital, the Health Sciences Center is a hub of academic, clinical and research activities.',
+            location: {
+                center: [-75.15191, 40.00617],
+                zoom: 16,
+                pitch: 61.70,
+                bearing: 61.70
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            mapInteractive: false,
+            callback: '',
+            onChapterEnter: [
+            //     {
+            //         layer: 'pcwnet',
+            //         opacity: 1
+            //     }
+            ],
+            onChapterExit: []
+        },
+        {
+            id: 'chapter5',
             alignment: 'right',
             hidden: false,
-            title: 'Third slide',
-            image: './images/image2.png',
-            description: 'Copy these sections to add to your story.',
+            title: 'Japan',
+            image: './images/image5.jpg',
+            website: '<a href="https://www.tuj.ac.jp/" target="https://www.tuj.ac.jp/">Website</a>', 
+            description: 'The Temple T flies proudly over this campus, which is the oldest and largest foreign university in Japan. Temple University, Japan Campus, takes pride in the top-quality, English-language education it offers. It also provides a unique opportunity for thousands of students from nearly 60 countries to earn American undergraduate and graduate degrees.',
             location: {
-                center: [-75.13331, 39.98741],
+                center: [139.67390, 35.64250],
+                zoom: 12,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            mapInteractive: true,
+            callback: '',
+            onChapterEnter: [
+            //     {
+            //         layer: 'pcwnet',
+            //         opacity: 1
+            //     }
+            ],
+            onChapterExit: []
+        }, 
+        {
+            id: 'chapter6',
+            alignment: 'left',
+            hidden: false,
+            title: 'Rome',
+            image: './images/image6.jpg',
+            website: '<a href="https://studyabroad.temple.edu/programs/temple-university-rome/semester-academic-year-summer" target="https://studyabroad.temple.edu/programs/temple-university-rome/semester-academic-year-summer">Website</a>', 
+            description: 'Housed in a historic, light-filled villa on the banks of the Tiber River, Temple Rome immerses students in the richness of Italian culture. An eclectic faculty, frequent field trips, small class sizes and internship opportunities provide students with an unforgettable study-abroad experience',
+            location: {
+                center: [12.48254, 41.90687],
                 zoom: 15,
                 pitch: 0,
                 bearing: 0
