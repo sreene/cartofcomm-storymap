4a4d5ddb1ea3fc60216327529947e49364579a85
fmvaldezg <fmvaldezg@yahoo.com>
2025-02-03 11:22:28 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {
     style: './map/style.json',
     showMarkers: true,
-    markerColor: '#192837',
+    markerColor: '#a41e35',
     inset: true, // if inset map is set to true, legend will be disabled.
     legend: false, // if legend is set to true, inset will be disabled.
     theme: 'dark',
@@ -10,18 +10,18 @@ var config = {
     bookmarks: true,
     chapterReturn: true,
     auto: false,
-    title: 'Temple University',
-    logo: "",
+    title: 'A tour of Temple University Campuses',
+    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Temple_University_Logo.svg/320px-Temple_University_Logo.svg.png",
     subtitle: 'Demo for the fall 2024 workshop',
-    byline: 'By Author',
+    byline: 'By Felipe Valdez',
     mobileview: '<div id="rotate-mobile"><em>For optimal viewing of this storytelling map on mobile, rotate your device to a horizontal orientation.</em><br><br><img src="images/device.png">', // to add custom messaging in the header for mobile devices
     footer: 'Source: source citations, etc.<br> Created using <a href="https://github.com/digidem/maplibre-storymap" target="_blank">MapLibre Storytelling</a> template.',
     chapters: [
         {
             id: 'chapter1',
-            alignment: 'right',
+            alignment: 'left',
             hidden: false,
-            title: 'Campus',
+            title: 'Main Campus',
             image: './images/image1.jpg',
             caption: 'Charles Library Building',
             website: '<a href="https://temple.maps.arcgis.com/apps/MapSeries/index.html?appid=9b76db43c88946cfa92d269463ac26dd" target="https://temple.maps.arcgis.com/apps/MapSeries/index.html?appid=9b76db43c88946cfa92d269463ac26dd">Historical map</a>',
@@ -29,9 +29,9 @@ var config = {
             //legend: '<span style="font-size: 0.85em;"><em>Legend content</em><br></span><img src="/images/legend1.png" style="display: block; margin-top: 10px; width: 100%; max-width: 200px;">',
             description: 'Temple’s bustling Main Campus crackles with energy, thanks to its location in the heart of Philadelphia, numerous new building projects and the 10,000 students who now live on or around our increasingly residential campus. On- and off-campus activities abound, including more than 300 student organizations; Division I sporting events; and nearby restaurants, shops and entertainment. <iframe width="100%" height="800px" src="https://temple.maps.arcgis.com/apps/MapSeries/index.html?appid=9b76db43c88946cfa92d269463ac26dd" frameborder="0" scrolling="no"></iframe>',
             location: {
-                center: [-75.159, 39.98265],
-                zoom: 17,
-                pitch: 0,
+                center: [-75.15552, 39.98203],
+                zoom: 16.57,
+                pitch: 49,
                 bearing: 0
             },
             mapAnimation: 'flyTo',
@@ -61,11 +61,11 @@ var config = {
             id: 'chapter2',
             alignment: 'right',
             hidden: false,
-            //title: 'Ambler',
+            title: 'Ambler',
             image: './images/image2.png',
             website: '<a href="https://ambler.temple.edu/" target="https://ambler.temple.edu/">Learn more</a>',
             //legend: '<span style="font-size: 0.85em;"><em>Legend content</em><br></span><img src="/images/legend1.png" style="display: block; margin-top: 10px; width: 100%; max-width: 200px;">',
-            //description: 'Beloved by generations of Temple students for its scenic beauty and nationally acclaimed gardens, Temple’s Ambler Campus fosters a close-knit community of students and faculty. Ambler students enjoy the full range of services that the suburban campus of a world-class university provides.',
+            description: 'Beloved by generations of Temple students for its scenic beauty and nationally acclaimed gardens, Temple’s Ambler Campus fosters a close-knit community of students and faculty. Ambler students enjoy the full range of services that the suburban campus of a world-class university provides.',
             location: {
                 center: [-75.19169, 40.16554],
                 zoom: 15.9,
@@ -104,7 +104,7 @@ var config = {
                 bearing: -36.70
             },
             mapAnimation: 'flyTo',
-            //rotateAnimation: true,
+            rotateAnimation: true,
             mapInteractive: false,
             callback: '',
             onChapterEnter: [],
@@ -215,33 +215,6 @@ var config = {
             //     }
             ],
             onChapterExit: []
-        },
-        {
-            id: 'chapter8',
-            alignment: 'left',
-            hidden: false,
-            //title: 'Workshop',
-            image: './images/image1.jpg',
-            //website: '<a href="https://studyabroad.temple.edu/programs/temple-university-rome/semester-academic-year-summer" target="https://studyabroad.temple.edu/programs/temple-university-rome/semester-academic-year-summer">Website</a>', 
-            description: 'Do you want to learn how to create your own story map like this? Visit our hands on <a href="https://felipevaldez.com/storytelling_maplibre_workshop/" target="https://felipevaldez.com/storytelling_maplibre_workshop/">online tutorial.</a>',
-            location: {
-                center: [-75.15515, 39.98185],
-                zoom:2.27,
-                pitch: 0,
-                bearing: 0
-            },
-            mapAnimation: 'flyTo',
-            rotateAnimation: false,
-            mapInteractive: true,
-            callback: '',
-            onChapterEnter: [
-            //     {
-            //         layer: 'pcwnet',
-            //         opacity: 1
-            //     }
-            ],
-            onChapterExit: []
         }
-
     ]
 };
